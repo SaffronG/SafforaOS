378ddadf2348038ddf094c965b2835fa91d5b035
SaffronGamer <saffrongamer167@gmail.com>
2025-02-10 09:36:39 -0700

    Added gitignore and added build configs

--- SafforaOS/src/main.rs
+++ SafforaOS/src/main.rs
@@ -1,14 +1,26 @@
-#![no_std]
-#![no_main]
+#![no_std] // no standard library
+#![no_main] // no main function
 
 use core::panic::PanicInfo;
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! { loop {} } // panic handler
+
+static HELLO: &[u8] = b"Hello, world!\n";
+
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start() -> ! { // defined entry point "_start"
+    let vga_buffer = 0xb8000 as *mut u8; // VGA buffer address
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // character
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // color
+        }
+    }
+
     loop {}
 }
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+// Unimplemented Functions (normally included with C standard library so we need to re-implement them)
+// >> memset, memcpy, memcmp
